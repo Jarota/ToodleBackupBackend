2088337f7666cd387cd771b041a7efa465de6487
Jarota <taitj@tcd.ie>
2020-11-13 21:18:25 +0000

    Now serves static frontend code

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+
 	jwtware "github.com/gofiber/jwt/v2"
 
 	"github.com/jarota/ToodleBackupBackend/handlers"
@@ -20,24 +22,32 @@ func main() {
 		ExposeHeaders: "Access-Control-Allow-Headers, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With",
 	}))
 
-	app.Get("/", handlers.HelloWorld)
-	app.Post("/register", handlers.Register)
-	app.Post("/login", handlers.Login)
+	app.Use(logger.New())
+
+	app.Static("/", "./frontend")
+	app.Static("/toodleredirect", "./frontend")
+
+	app.Post("/api/register", handlers.Register)
+	app.Post("/api/login", handlers.Login)
 
 	app.Use(jwtware.New(jwtware.Config{
 		SigningKey: []byte(os.Getenv("SECRET")),
 		ContextKey: "userInfo",
 	}))
 
-	app.Get("/getUser", handlers.GetUser)
-	app.Post("/logout", handlers.Logout)
-	app.Put("/connToodledo", handlers.ConnToodledo)
-	app.Put("/connCloudStorage", handlers.ConnCloudStorage)
-	app.Put("/setBackupFrequency", handlers.SetBackupFrequency)
+	app.Get("/api/getUser", handlers.GetUser)
+	app.Post("/api/logout", handlers.Logout)
+	app.Put("/api/connToodledo", handlers.ConnToodledo)
+	app.Put("/api/connCloudStorage", handlers.ConnCloudStorage)
+	app.Put("/api/setBackupFrequency", handlers.SetBackupFrequency)
 
-	app.Get("/randomString", handlers.RandomString)
+	app.Get("/api/randomString", handlers.RandomString)
 
-	app.Listen(":3000")
+	err := app.Listen(":8080")
+
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	// err := db.Client.Disconnect(context.TODO())
 
